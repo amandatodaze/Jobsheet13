cc4ececa058e440cc7047158bd27638fd166613a
amandatodaze <amandatodaze@gmail.com>
2024-12-04 14:38:10 +0700

    bikin fungsi

--- KRS.java
+++ KRS.java
@@ -1,4 +1,16 @@
 public class KRS {
+    public class tambahKRS {
+            
+    }
+    
+    public class tampilkanKRS {
+        
+    }
+
+    public class analisisDataKRS {
+        
+    }
+    
     public static void main(String[] args) {
         
     }
